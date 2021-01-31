57e086cd012c83b957a26421dc8897cff7907928
xrdrx <ryd3rr@gmail.com>
2021-01-31 16:40:09 +0300

    Json parser change

--- src/main/java/operations/json/JsonParser.java
+++ src/main/java/operations/json/JsonParser.java
@@ -14,28 +14,32 @@ import java.util.List;
 
 public class JsonParser {
 
+    private Gson gson;
+
+    public JsonParser() {
+        this.gson = setUpTicketGson();
+    }
+
     public List<Ticket> getTicketsFromFileOrDefault(String filePath) throws FileNotFoundException {
         if (filePath.isEmpty()) {
             return getDefaultTicketsList();
         }
         FileReader reader = new FileReader(filePath);
-        Gson gson = setUpTicketGson();
         return gson.fromJson(reader, Tickets.class).getTickets();
     }
 
     public List<Ticket> getDefaultTicketsList() {
         InputStream resource = getClass().getClassLoader().getResourceAsStream("tickets.json");
         InputStreamReader in = new InputStreamReader(resource);
-        Gson gson = setUpTicketGson();
         return gson.fromJson(in, Tickets.class).getTickets();
     }
 
     private Gson setUpTicketGson() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yy");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yy");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("H:m");
         return new GsonBuilder()
                 .registerTypeAdapter(LocalDate.class, (JsonDeserializer<LocalDate>) (json, typeOfT, context) ->
-                        LocalDate.parse(json.getAsString(), formatter))
+                        LocalDate.parse(json.getAsString(), dateFormatter))
                 .registerTypeAdapter(LocalTime.class, (JsonDeserializer<LocalTime>) (json, typeOfT, context) ->
                         LocalTime.parse(json.getAsString(), timeFormatter))
                 .create();
